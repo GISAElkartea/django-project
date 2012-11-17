b390bafdfe3d5e795c00481dc4b3247067649ade
Unai Zalakain <contact@unaizalakain.info>
2012-11-17 16:23:13 +0100

    add template summary

--- django_project/pastertemplates.py
+++ django_project/pastertemplates.py
@@ -2,6 +2,7 @@ from paste.script.templates import Template
 
 
 class DjangoProjectTemplate(Template):
+    summary = 'Django project template'
     vars = []
     use_cheetah = True
     _template_dir = 'project_template'
